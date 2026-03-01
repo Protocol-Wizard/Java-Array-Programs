ae17e1ee9a364e8b9c6b1ef5dc39d6cf945fc6e3
SAM-AK-ASH <samakashramesh2007@gmail.com>
2026-03-01 23:18:27 +0530

    ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,17 +1,15 @@
-class ReverseArray {
-    public static void main(String[] args) {
+public class ReverseArray{
+    public static void main(String[] args){
         int[] a = {10, 20, 30, 40, 50};
         int i = 0, j = a.length - 1;
-
-        while (i < j) {
+        while (i < j){
             int temp = a[i];
             a[i] = a[j];
             a[j] = temp;
             i++;
             j--;
         }
-
-        for (int x : a) {
+        for (int x : a){
             System.out.print(x + " ");
         }
     }
