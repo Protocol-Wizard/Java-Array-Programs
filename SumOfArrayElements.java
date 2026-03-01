162c213fca77b521e4cf9ae12a5c717082962cef
SAM-AK-ASH <samakashramesh2007@gmail.com>
2026-03-01 23:23:54 +0530

    SumOfArrayElements.java

--- SumOfArrayElements.java
+++ SumOfArrayElements.java
@@ -1,8 +1,8 @@
-class SumOfArrayElements {
-    public static void main(String[] args) {
+public class SumOfArrayElements{
+    public static void main(String[] args){
         int[] a = {5, 10, 15, 20};
         int sum = 0;
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++){
             sum += a[i];
         }
         System.out.println("Sum = " + sum);
