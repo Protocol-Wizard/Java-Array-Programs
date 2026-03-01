bfd0d40ad05fb72921af92c088bf391b0a7b6dee
SAM-AK-ASH <samakashramesh2007@gmail.com>
2026-03-01 22:58:39 +0530

    LengthOfArrays.java

--- LengthOfArrays.java
+++ LengthOfArrays.java
@@ -1,5 +1,5 @@
-class LengthOfArrays {
-    public static void main(String[] args) {
+public class LengthOfArrays{
+    public static void main(String[] args){
         char[] charArray1 = new char[5];       
         char[] charArray2 = {'a', 'b', 'c', 'd', 'e'};
         System.out.println("Length of charArray1: " + charArray1.length);
