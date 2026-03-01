444bdb25beab315942a9155eb60c025ffed5cf28
SAM-AK-ASH <samakashramesh2007@gmail.com>
2026-03-01 22:51:44 +0530

    EvenOddCount.java

--- EvenOddCount.java
+++ EvenOddCount.java
@@ -1,17 +1,16 @@
-class EvenOddCount {
-    public static void main(String[] args) {
+public class EvenOddCount{
+    public static void main(String[] args){
         int[] a = {1, 2, 3, 4, 5, 6};
-        int even = 0, odd = 0;
-
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] % 2 == 0){
-			even++;
+        int even = 0; 
+		int odd = 0;
+        for (int i=0;i<a.length;i++){
+            if (a[i]%2==0){
+			    even++;
 			}
             else{
-			odd++;
+			    odd++;
 			}
         }
-
         System.out.println("Even integers: " + even);
         System.out.println("Odd integers: " + odd);
     }
